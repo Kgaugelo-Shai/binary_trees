1c60ff661818ddb3d65af52bea1cdb234e5979f9
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-03-29 05:07:23 +0200

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,13 +25,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left = 0;
-        size_t right = 0;
+	size_t left = 0;
+	size_t right = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-        right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-        return ((left > right) ? left : right);
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	return ((left > right) ? left : right);
 }
